8f99c2e2f0aefeae83392840959f5afff0187edb
Manfred Graf <manfred.graf91@gmail.com>
2024-11-07 16:16:37 +0100

    chore: initialized project

---
+++ src/test/kotlin/com/tractive/pettracking/PetTrackingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tractive.pettracking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetTrackingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
